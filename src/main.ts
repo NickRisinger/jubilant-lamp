8ff74cbec3c9933270dff5cf97b90e9294b7d9ae
NickRisinger <boozy044@gmail.com>
2025-03-01 12:52:50 +0300

    refactor: remove base styles

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import 'element-plus/dist/index.css';
-import './style.css';
 
 const pinia = createPinia();
 const app = createApp(App);
